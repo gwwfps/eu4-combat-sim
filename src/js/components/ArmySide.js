85391311c55379d4e54b47f25c153a6ae220b770
David/Zitao Zhang <gwwfps@gmail.com>
2015-02-08 23:13:39 -0500

    input field refactor

--- src/js/components/ArmySide.js
+++ src/js/components/ArmySide.js
@@ -7,6 +7,7 @@ import SimulationActions from '../actions/SimulationActions.js';
 import SimulationStore from '../stores/SimulationStore.js';
 
 const React = require('react/addons');
+const _ = require('lodash');
 
 
 const _getStateFromStore = (side) => {
@@ -24,71 +25,75 @@ export default React.createClass({
     return this.props.side.toLowerCase();
   },
 
+  _linkedField(name, additionalProps) {
+    return input(_.extend({ valueLink: this.linkState(name), type: 'number' }, additionalProps || {}));
+  },
+
   render() {
     return div({ className: 'one-half column ' + this._getSideLowerCase() },
       gf(
         gfSubform(this._getSideLowerCase(),
           gfRow(
             gfField('Discipline (%)',
-              input({ valueLink: this.linkState('discipline'), type: 'number', min: 50, max: 200 })
+              this._linkedField('discipline', { min: 50, max: 200 })
             ),
             gfField('Morale',
-              input({ valueLink: this.linkState('morale'), type: 'number', min: 0.01, max: 15, step: '0.01' })
+              this._linkedField('morale', { min: 0.01, max: 15, step: '0.01' })
             ),
             gfField('Mil. Tactics',
-              input({ valueLink: this.linkState('tactics'), type: 'number', min: 0.5, max: 3.5, step: '0.25' })
+              this._linkedField('tactics', { min: 0.5, max: 3.5, step: '0.25' })
             ),
             gfField('Combat Width',
-              input({ valueLink: this.linkState('combatWidth'), type: 'number', min: 15, max: 40 })
+              this._linkedField('combatWidth', { min: 15, max: 40 })
             )
           ),
           gfRow(
             gfField('Cavalry support ratio (%)',
-              input({ valueLink: this.linkState('cavRatio'), type: 'number', min: 50, max: 100, step: 10 })
+              this._linkedField('cavRatio', { min: 50, max: 100, step: 10 })
             ),
             gfField('Improved flanking range (%)',
-              input({ valueLink: this.linkState('improvedFlanking'), type: 'number', min: 0, max: 125, step: 25 })
+              this._linkedField('improvedFlanking', { min: 0, max: 125, step: 25 })
             )
           ),
           gfRow(
             gfField('Infantry combat ability (%)',
-              input({ valueLink: this.linkState('infCombatAbility'), type: 'number', min: 0, max: 100 })
+              this._linkedField('infCombatAbility', { min: 0, max: 100 })
             ),
             gfField('Cavalry combat ability (%)',
-              input({ valueLink: this.linkState('cavCombatAbility'), type: 'number', min: 0, max: 100 })
+              this._linkedField('cavCombatAbility', { min: 0, max: 100 })
             ),
             gfField('Artillery combat ability (%)',
-              input({ valueLink: this.linkState('artCombatAbility'), type: 'number', min: 0, max: 100 })
+              this._linkedField('artCombatAbility', { min: 0, max: 100 })
             )
           ),
           gfRow(
             gfField('Infantry +fire',
-              input({ valueLink: this.linkState('infFire'), type: 'number', min: 0, max: 10, step: 0.01 })
+              this._linkedField('infFire', { min: 0, max: 10, step: 0.01 })
             ),
             gfField('Cavalry +fire',
-              input({ valueLink: this.linkState('cavFire'), type: 'number', min: 0, max: 10, step: 0.01 })
+              this._linkedField('cavFire', { min: 0, max: 10, step: 0.01 })
             ),
             gfField('Artillery +fire',
-              input({ valueLink: this.linkState('artFile'), type: 'number', min: 0, max: 10, step: 0.01 })
+              this._linkedField('artFile', { min: 0, max: 10, step: 0.01 })
             )
           ),
           gfRow(
             gfField('Infantry +shock',
-              input({ valueLink: this.linkState('infShock'), type: 'number', min: 0, max: 10, step: 0.01 })
+              this._linkedField('infShock', { min: 0, max: 10, step: 0.01 })
             ),
             gfField('Cavalry +shock',
-              input({ valueLink: this.linkState('cavShock'), type: 'number', min: 0, max: 10, step: 0.01 })
+              this._linkedField('cavShock', { min: 0, max: 10, step: 0.01 })
             ),
             gfField('Artillery +shock',
-              input({ valueLink: this.linkState('artShock'), type: 'number', min: 0, max: 10, step: 0.01 })
+              this._linkedField('artShock', { min: 0, max: 10, step: 0.01 })
             )
           ),
           gfRow(
             gfField('General Fire',
-              input({ valueLink: this.linkState('genFire'), type: 'number', min: 0, max: 6 })
+              this._linkedField('genFire', { min: 0, max: 6 })
             ),
             gfField('General Shock',
-              input({ valueLink: this.linkState('genShock'), type: 'number', min: 0, max: 6 })
+              this._linkedField('genShock', { min: 0, max: 6 })
             )
           )
         ),
